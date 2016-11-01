c9bd69885c267a1374e016a524b5e57fc8afaf66
Huy Vuong <huy-vuong@users.noreply.github.com>
2016-10-31 23:18:39 -0700

    #2 Scaffold Android project using Android Studio

---
+++ myappportfolio/src/main/java/com/huyvuong/adn/myappportfolio/PortfolioActivity.java
@@ -0,0 +1,13 @@
+package com.huyvuong.adn.myappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PortfolioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portfolio);
+    }
+}
